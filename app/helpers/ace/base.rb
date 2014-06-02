d4216462e55e386863a2d43771f60931b0c599aa
cravista <gcravista@gmail.com>
2014-06-01 22:48:14 -0300

    Fixed some errors with editor
    Integration between editor and terminal: when terminal on
    focus, editor cannot state in focus. Find a way to fix

--- app/helpers/ace/base.rb
+++ app/helpers/ace/base.rb
@@ -21,8 +21,8 @@ module AceHelper
     
     protected
 
-    BASE_RUN =  "gac.run (err, js) -> if not err then window.terminal.print 'render: DONE' else t.print err\n"
-    BASE_CLEAR = "gac.clean (err, js) -> if not err then window.terminalr.print 'stop: DONE' else t.print err\n"
+    BASE_RUN =  " gac.run (err, js) -> window.terminal.type('# Running gibberish.js', '#0FA')\n"
+    BASE_CLEAR = "gac.clean (err, js) -> window.terminal.type('# Cleaning gibberish.js', '#0FA')\n"
 
     RENDER_OPTIONS = {
       :lang => MODE,                       # The running-language of web-application
@@ -34,47 +34,48 @@ module AceHelper
         },
         :stop => {
           :bindKeys => "win: 'Ctrl-.', linux: 'Ctrl-.', mac: 'Command-S'",
-          :exec => "(e) ->\n  #{BASE_CLEAR}"
+          :exec => "(e) -> #{BASE_CLEAR}"
         }
       },
       :buttons => {
-        :render =>"$('#render').click (e)->\n  #{BASE_RUN}",
-        :stop => "$('#stop').click (e)->\n  #{BASE_CLEAR}"
+        :render =>"$('#render').click (e)-> #{BASE_RUN}",
+        :stop => "$('#stop').click (e)-> #{BASE_CLEAR}"
       }
     }
 
     COMMENT_MESSAGE = """
-                       # ======================================================================================
-                       # Hi, this is an ace editor integrated with terminal                    
-                       # 
-                       # All previous posts in index page                                     
-                       # show a way to use coffee-script language                           \/ \\
-                       # See old ones if you do not know anything                          \/   \\
-                       #                                                                  \/_____\\
-                       # You can use coffee-script here to change the above code         \/       \\
-                       # Think in a way to make music                                   \/         \\
-                       # When done, you can use hotkeys to render or stop music:        ------------            
-                       # - Render audio:                                               a            A
-                       #   - Mac: Command + Enter                                     aaa          AAA
-                       #   - Linux / Windows: Ctrl + Enter                           aa aa        AA AA  
-                       # - Stop audio:                                              aa   aa      AA   AA
-                       #   - Mac: Command + .                                      aaa   aaa    AAA   AAA
-                       #   - Linux / Window: Ctrl + .                             aa       aa  AA       AA
-                       #                                                          AA       AA  aa       aa
-                       # Or you can use 'render' or 'stop' commands               AA       AA  aa       aa
-                       # Above you (buttons) or type them in terminal            aaaaaaaaaaaaaAAAAAAAAAAAAA   
-                       # If you find any issue, please contact me in:            AAAAAAAAAAAAAaaaaaaaaaaaaa 
-                       #                                                        aaaaaaaaaaaaaAAAAAAAAAAAAAAA
-                       # https://github.com/jahpd/yaknowboutblogmusic/issues   aaa          aAA           AAA        
-                       #                                                      aaaaa        aaAAA         AAAAA
-                       # If you want to contribute with any code, fork it!   aaaaaaa      aaaAAAA       AAAAAAA
-                       #
-                       # If you want to reply, some sugestion, critic  
-                       # send email for xxx@yyyy.zzz  
-                       # or join irc.freenode.net #labmacambira
-                       #                                              
-                       # Happy live-coding!
-                       # (DESTROY ME AND USE EDITOR!)
-                       # ======================================================================================\n"""
+# ======================================================================================
+# Hi, this is an ace editor integrated with terminal                    
+# 
+# All previous posts in index page                                     
+# show a way to use coffee-script language                           \/ \\
+# See old ones if you do not know anything                          \/   \\
+#                                                                  \/_____\\
+# You can use coffee-script here to change the code below         \/       \\
+# !Think in a way to make music!                                 \/         \\
+# When done, you can use hotkeys to render or stop music:        ------------            
+# - Render audio:                                               a            A
+#   - Mac: Command + Enter                                     aaa          AAA
+#   - Linux / Windows: Ctrl + Enter                           aa aa        AA AA  
+# - Stop audio:                                              aa   aa      AA   AA
+#   - Mac: Command + .                                      aaa   aaa    AAA   AAA
+#   - Linux / Window: Ctrl + .                             aa       aa  AA       AA
+#                                                          AA       AA  aa       aa
+# Or you can use 'render' or 'stop' commands               AA       AA  aa       aa
+# with buttons above or type them in terminal             aaaaaaaaaaaaaAAAAAAAAAAAAA   
+# at your right side.                                     0    .25   0.5    .75    1
+#                                                         AAAAAAAAAAAAAaaaaaaaaaaaaa 
+# If you find any issue, please contact me in:           aaaaaaaaaaaaaAAAAAAAAAAAAAAA
+# https://github.com/jahpd/yaknowboutblogmusic/issues   aaa          aAA           AAA        
+#                                                      aaaaa        aaAAA         AAAAA
+# If you want to contribute with any code, fork it!   aaaaaaa      aaaAAAA       AAAAAAA
+#
+# If you want to reply, some sugestion, critic  
+# send email for xxx@yyyy.zzz  
+# or join irc.freenode.net #labmacambira
+                                              
+# Happy live-coding!
+# (DESTROY ME AND USE EDITOR!)
+# ======================================================================================\n"""
   end
 end
