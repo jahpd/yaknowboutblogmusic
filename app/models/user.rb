d9c8df3d0333b9dccb42cdb393e997598e049252
cravista <gcravista@gmail.com>
2014-08-21 10:25:07 -0300

    modificado background
    not fundamental, but attractive

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,20 +8,29 @@ class User < ActiveRecord::Base
   # This is in addition to a real persisted field like 'name'
   attr_accessor :login
 
+  # This validates name in with only letters
   validate :name,{
     :uniqueness => { :case_sensitive => false },
     :format => { with: /\A[a-zA-Z]+\z/ },
     :message => "only allows letters"
   }
 
-  validates_format_of :email, :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+  # This Validates email
+  # [letters/numbers@something.som]
+  # see more on https://en.wikipedia.org/wiki/Regular_expression
+  validates_format_of(:email, {
+    :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+  })
 
   # is the correct method you override with the code above
-  # self.find_for_database_authentication(warden_conditions)
+  # for authenticate user
   def self.find_first_by_auth_conditions(warden_conditions)
    conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions).where([
+        "lower(name) = :value OR lower(email) = :value", 
+        {:value => login.downcase }
+      ]).first
     else
       where(conditions).first
     end
